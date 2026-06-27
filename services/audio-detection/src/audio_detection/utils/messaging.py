63bcec6306f5b14b6961108d8fdca10cba028c5d
tjmorton91 <tom.morton91@gmail.com>
2026-06-27 17:39:11 +0100

    main - added rabbitmq and s3 utils

---
+++ services/audio-detection/src/audio_detection/utils/messaging.py
@@ -0,0 +1,55 @@
+"""Utilites for RabbitMQ"""
+
+import logging
+
+import pika
+
+from .environment import get_environment
+
+config = get_environment()
+logger = logging.getLogger(__name__)
+
+
+def ensure_rabbitmq_queue() -> None:
+    """Declare RabbitMQ queue if it doesn't exist.
+
+    Raises
+    ------
+    Exception
+        If queue declaration fails
+    """
+    logger.info("Bootstrapping RabbitMQ")
+
+    # Get values from environment
+    host = config["rabbitmq_host"]
+    port = config["rabbitmq_port"]
+    queue_name = config["rabbitmq_queue_name"]
+    user = config["rabbitmq_user"]
+    password = config["rabbitmq_password"]
+
+    logger.debug("Using queue name %s", queue_name)
+
+    credentials = pika.PlainCredentials(user, password)
+
+    connection_params = pika.ConnectionParameters(
+        host=host, port=port, credentials=credentials
+    )
+
+    try:
+        # TODO: (tjm) Is there an asyncio equivalent of this?
+        connection = pika.BlockingConnection(connection_params)
+        channel = connection.channel()
+
+        # Declaring a queue is idempotent, so we can run this on startup
+        #   every time without problem
+        channel.queue_declare(queue=queue_name, durable=True)
+
+        # TODO: (tjm) Not the end of the world, but we could re-use this
+        #   connection handle I think
+        logger.debug("RabbitMQ queue ready, closing connection")
+        connection.close()
+    except Exception as e:
+        logger.error("Failed to bootstrap RabbitMQ, %s", e)
+        raise
+
+    logger.info("Finished bootstrapping RabbitMQ")
---
+++ services/audio-detection/src/audio_detection/utils/storage.py
@@ -0,0 +1,59 @@
+"""Utilites for S3 Storage"""
+
+import logging
+
+import boto3
+from botocore.exceptions import ClientError
+
+from .environment import get_environment
+
+config = get_environment()
+logger = logging.getLogger(__name__)
+
+
+def ensure_s3_bucket() -> None:
+    """Create S3 bucket if it doesn't exist.
+
+    Raises
+    ------
+    Exception
+        If bucket creation fails for reasons other than already existing
+    """
+    logger.info("Bootstrapping S3 bucket")
+
+    # Get values from environment
+    endpoint_url = config["aws_endpoint_url"]
+    access_key_id = config["aws_access_key_id"]
+    secret_access_key = config["aws_secret_access_key"]
+    region = config["aws_region"]
+    bucket_name = config["s3_bucket_name"]
+
+    logger.debug("Using S3 bucket name %s", bucket_name)
+
+    s3_client = boto3.client(
+        "s3",
+        endpoint_url=endpoint_url,
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_access_key,
+        region_name=region,
+    )
+
+    try:
+        s3_client.head_bucket(Bucket=bucket_name)
+        logger.debug("S3 bucket %s already exists", bucket_name)
+    except ClientError as e:
+        # If we've got a 404 back, create the bucket
+        error_code = e.response["Error"]["Code"]
+        if error_code == "404":
+            logger.debug("S3 bucket doesn't exist, creating bucket...")
+            try:
+                s3_client.create_bucket(bucket_name)
+                logger.debug("S3 bucket created")
+            except ClientError as create_error:
+                logger.error("Failed to create S3 bucket, %s", create_error)
+                raise
+        else:
+            logger.error("Failed to check for S3 bucket")
+            raise
+
+    logger.info("Finished bootstrapping S3 bucket")
